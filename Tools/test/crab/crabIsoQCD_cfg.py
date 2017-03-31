01037032480217421ca571b2e101ffb8f1bb49da
brochero <javier.brochero.cifuentes@cern.ch>
2017-03-31 15:39:12 +0200

    Correct config for QCD

--- Tools/test/crab/crabIsoQCD_cfg.py
+++ Tools/test/crab/crabIsoQCD_cfg.py
@@ -3,15 +3,15 @@ config = Configuration()
 
 config.section_('General')
 config.General.transferLogs = True
-# config.General.requestName = 'QCD_Pt-15to20_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
-# config.General.requestName = 'QCD_Pt-20to30_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
-# config.General.requestName = 'QCD_Pt-30to50_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
-# config.General.requestName = 'QCD_Pt-50to80_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
-# config.General.requestName = 'QCD_Pt-80to120_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_v3'
-# config.General.requestName = 'QCD_Pt-120to170_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
-# config.General.requestName = 'QCD_Pt-170to300_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_v3'
-# config.General.requestName = 'QCD_Pt-300to470_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
-config.General.requestName = 'QCD_Pt-470to600_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26'
+#config.General.requestName = 'QCD_Pt-15to20_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-20to30_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-30to50_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-50to80_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-80to120_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-120to170_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-170to300_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+#config.General.requestName = 'QCD_Pt-300to470_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
+config.General.requestName = 'QCD_Pt-470to600_FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26_Nmu1'
 
 config.section_('JobType')
 config.JobType.pluginName  = 'Analysis'
@@ -22,23 +22,23 @@ config.JobType.pyCfgParams = ['globalTag=81X_upgrade2017_realistic_v26',
                               'runOnMC=True',
                               'hltPathFilter=all',
                               'minMuPt=10.0',
-                              'minNMu=2'
+                              'minNMu=1'
                ]
 config.JobType.allowUndistributedCMSSW = True  # To fix cmssw releases
 
 config.section_('Data')
-# config.Data.inputDataset = '/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-20to30_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-30to50_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-50to80_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
-# config.Data.inputDataset = '/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-20to30_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-30to50_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-50to80_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
+#config.Data.inputDataset = '/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
 config.Data.inputDataset = '/QCD_Pt-470to600_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_81X_upgrade2017_realistic_v26-v1/AODSIM'
 
 config.Data.splitting    = 'FileBased'
-config.Data.unitsPerJob  = 5
+config.Data.unitsPerJob  = 10
 config.Data.inputDBS     = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 # config.Data.ignoreLocality  = True
 # config.Data.allowNonValidInputDataset = True
